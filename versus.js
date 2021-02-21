17e6bfbc74015d2196ff5aa13d75f3473cc85767
MadJangE00 <jiniixz@naver.com>
2021-02-21 17:25:26 +0900

    versus

--- versus.js
+++ versus.js
@@ -2,11 +2,11 @@ var coke = {'name': 'coke', 'point': 100}
 var pep = {'name': 'pep', 'point' : 120}
 
 function battle (a, b){
-  if (a[point] - b[point]) > 0){
+  if (a["point"] - b["point"]) > 0){
     console.log(a + ' win')
-  } else if (a[point] - b[point]) < 0){
+  } else if (a["point"] - b["point"]) < 0){
     console.log(b + ' win')
   } else {
-    console.log(draw)
+    console.log('draw')
   }
 }
